f713165daf2b52c6adc27c255d7cce04bcbc0739
Gani Ozturk <ganioztrk@gmail.com>
2023-09-15 20:40:39 +0300

    identity

---
+++ IdentityProject/UserIdentity/UserIdentity/Controllers/AccountController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using UserIdentity.Identity;
+using UserIdentity.Models;
+
+namespace UserIdentity.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public AccountController()
+        {
+            var userStore = new UserStore<ApplicationUser>(new IdentityDataContext());
+            _userManager = new UserManager<ApplicationUser>(userStore);
+
+        }
+
+        // GET: Account
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult Register()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Register(UserModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                var user = new ApplicationUser()
+                {
+                    UserName = model.Email,
+                    Email = model.Email
+                };
+                var result = _userManager.Create(user, model.Password);
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Login");
+                }
+                else
+                {
+                    foreach (var item in result.Errors)
+                    {
+                        ModelState.AddModelError("","error");
+                    }
+                }
+            }
+            return View(model);
+        }
+    }
+}
